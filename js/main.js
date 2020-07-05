859148463a3d5f93ddcee1f65de00ecfc815c5cc
Ciratoku <Ciratoku@users.noreply.github.com>
2020-07-05 22:58:54 +0300

    Ability to watch pdf files

--- js/main.js
+++ js/main.js
@@ -5,17 +5,84 @@ let pdfDoc = null,
  pageIsRendering = false,
  pageNumIsPending = null;
 
-const scale = 1.5,
+const scale = 2,
  canvas = document.querySelector('#pdf-render'),
  ctx = canvas.getContext('2d');
 
 // Render the page
 const renderPage = num => {
+    pageIsRendering = true;
 
+    // get the page
+
+    pdfDoc.getPage(num).then(page => {
+        //Set scale
+        const viewport = page.getViewport({scale});
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+        
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+
+            if(pageNumIsPending !== null){
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        });
+
+        //cur page
+        document.querySelector('#page-num').textContent = num;
+    });
+};
+
+//Check for pages rendering
+const queueRenderPage = num => {
+    if(pageIsRendering){
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+}
+
+// show prev page
+const showPrevPage = () => {
+    if(pageNum <= 1){
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+// show next page
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages){
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
 }
 
 // Get Document
 pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
     pdfDoc = pdfDoc_;
-    console.log(pdfDoc);
-});
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+    renderPage(pageNum);
+})
+    .catch(err => {
+        //Display error
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('body').insertBefore(div, canvas);
+        //Remove top bar
+        document.querySelector('.top-bar').style.display = 'none';
+    });
+
+// Button events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
